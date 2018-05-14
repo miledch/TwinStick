a308ec0b975115ff192cc3e883b14d80dffd02b8
DESKTOP-MILED\miled <miledch@gmail.com>
2018-05-14 13:15:50 -0400

    Using CrossPlatformInput

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        print("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+}
